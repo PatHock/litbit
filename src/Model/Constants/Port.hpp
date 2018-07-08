8cceea6471713026c2dc3861154b613d005037b7
PatHock <phock@ycp.edu>
2018-07-08 16:55:00 -0400

    Change BLE RST constant to work with breadboard

--- src/Model/Constants/Port.hpp
+++ src/Model/Constants/Port.hpp
@@ -24,8 +24,9 @@ const uint8_t ACCEL_INT_2 = 9;      // uP pin 29, Arduino/ItsyBitsy pin D9
 const uint8_t BLE_ACTIVE = 11;      // uP pin 12, Arduino/ItsyBitsy pin D11
 const uint8_t BLE_REQN = 7;         // uP pin 1, Arduino/ItsyBitsy pin D7
 const uint8_t BLE_RDYN = 0;         // uP pin 20, Arduino/ItsyBitsy pin D0
-//FIXME: verify that constant below is correct
-const uint8_t BLE_RST = RXLED0;     // uP pin 8, Arduino/ItsyBitsy pin "RXLED"
+//FIXME: commented code should work for PCB, below is for breadboard...
+const uint8_t BLE_RST = 5;
+// const uint8_t BLE_RST = RXLED0;     // uP pin 8, Arduino/ItsyBitsy pin "RXLED"
 
 // EEPROM (NONE)
 
