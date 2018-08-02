b86dfe29ffe0ab066c5b4541e453ea81b7699905
PatHock <phock@ycp.edu>
2018-08-01 20:18:50 -0400

    migrate eeprom to controller-model

---
+++ src/Controller-Model/Eeprom/Eeprom.hpp
@@ -0,0 +1,35 @@
+/**
+ * Eeprom.hpp
+ * 24FC1025 EEPROM
+ * 
+ * Patrick Hock 
+ * August 1, 2018
+ */
+
+#ifndef EEPROM_HPP
+#define EEPROM_HPP
+
+#include <Arduino.h>
+
+class Eeprom
+{
+    public:
+        
+        ~Eeprom(void); // Deconstructor
+        
+        // Returns the instance of EepromController
+        static Eeprom* getInstance();
+        
+        // Initializes, since constructor cannot be called (to enforce singletons)
+        void init(void);
+
+    
+    private:
+        Eeprom(void){};     // Constructor
+
+        // Pointer to the single instance of EepromController 
+        static Eeprom* _pInstance;
+        
+};
+
+#endif  // EEPROM_HPP
