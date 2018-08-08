70e172f95cc4aac9e07d61da6bf0ae7bb658a16c
PatHock <phock@ycp.edu>
2018-08-07 21:44:15 -0400

    Clean up code, lower activity thresholds, increase sample freq to 8 hz

--- src/Controller-Model/Accel/Accel.cpp
+++ src/Controller-Model/Accel/Accel.cpp
@@ -10,34 +10,27 @@
 
 Accel *Accel::_pInstance = NULL;
 
-/**
- * Deconstructor
- */
-Accel::~Accel(void)
-{
-    delete adxl;
-}
-
-/**
- * Initialization
+/** 
+ * @brief  Initialization function. Called at boot time since constructor is private
+ * @note   
+ * @retval None
  */
 void Accel::init(void)
 {
     accelRange = ADXL_RANGE_4G;
-
     adxl = new ADXL345();
     
-    // Enable link bit, sample rate 1hz, Measure mode
-    writeToAddress(ADXL345_POWER_CTL, ADXL_MEASURE_BIT | ADXL_LINK_BIT | ADXL_WAKEUP_1HZ); 
+    // Enable link bit, sample rate 8hz, Measure mode
+    writeToAddress(ADXL345_POWER_CTL, ADXL_MEASURE_BIT | ADXL_LINK_BIT | ADXL_WAKEUP_8HZ); 
 
     setAccelRange(accelRange);
     adxl->set_bw(0x7);             // 12.5 Hz
     adxl->setInterruptLevelBit(0); // interrupts are active high
 
-    adxl->setActivityThreshold(22);   // 62.5mg per increment (verify that this is true)
+    adxl->setActivityThreshold(17);   // 62.5mg per increment (verify that this is true)
     adxl->setActivityXYZ(1, 1, 1);    // Enable activity detection in all axes
     adxl->setTimeInactivity(3);       // set inactivity timeout period, in seconds
-    adxl->setInactivityThreshold(18); // Set inactivity threshold, 62.5mg per increment
+    adxl->setInactivityThreshold(16); // Set inactivity threshold, 62.5mg per increment
     adxl->setInactivityXYZ(1, 1, 1);  // Enable inactivity detection
 
     readFromAddress(ADXL345_POWER_CTL);
@@ -49,18 +42,145 @@ void Accel::init(void)
     // adxl -> setDoubleTapWindow(200);       // 1.25 ms per increment
     // adxl -> setTapDetectionOnXYZ(true, true, true);
 
-    writeToAddress(ADXL345_INT_MAP, 0x2);              //map watermark to INT2 pin
-    adxl->setImportantInterruptMapping(1, 2, 1, 1, 2); // map double tap, inactivity to INT2
+    mapInterrupts();
 
     setupFIFO(ADXL_FIFO_MODE_FIFO, ADXL_WATERMARK_SIZE); // Set watermark to 30 samples, FIFO mode
 
+    enableInterrupts();
+}
+
+/** 
+ * @brief  Map Interrupts to ADXL interrupt pins (INT1 or INT2)
+ * @note   Must be called before enableInterrupts() in init()
+ * @retval None
+ */
+void Accel::mapInterrupts(void)
+{
+    writeToAddress(ADXL345_INT_MAP, 0x2);              //map watermark to INT2 pin
+    adxl->setImportantInterruptMapping(1, 2, 1, 1, 2); // map double tap, inactivity to INT2
+}
+
+/** 
+ * @brief  Enables Interrupts.
+ * @note   Call this last in init()
+ * @retval None
+ */
+void Accel::enableInterrupts(void)
+{
     writeToAddress(ADXL345_INT_ENABLE, 0x2); // Enable watermark interrupt
     adxl->InactivityINT(1);                  // Enable Inactivity Interrupt
     adxl->ActivityINT(1);                    // Enable Activity Interrupt
 }
 
-/**
- * Singleton Manager
+/** 
+ * @brief  Sets FIFO mode and watermark. Trigger is disabled.
+ * @note   Must be called before enabling interrupts in init()
+ * @param  mode: FIFO mode (Bypass, FIFO, Stream, or Trigger)
+ * @param  watermark: Number of samples in the FIFO until the watermark interrupt is trigger
+ * @retval None
+ */
+void Accel::setupFIFO(uint8_t mode, uint8_t watermark)
+{
+    // Set the mode and watermark
+    writeToAddress((uint8_t)0x38, ((uint8_t)(mode << 6) | (uint8_t)(watermark & 0x1F)) & ~(uint8_t)(0x20));
+}
+
+/** 
+ * @brief  Reads from specified address of ADXL, updates value in adxlReg
+ * @note   
+ * @param  addr: 8-bit address of ADXL register
+ * @retval None
+ */
+void Accel::readFromAddress(uint8_t addr)
+{
+    Wire.beginTransmission((uint8_t)0x53); // Begin transmission with ADXL
+    Wire.write((uint8_t)addr);             // Slave address to be read from
+    Wire.endTransmission();
+
+    Wire.beginTransmission((uint8_t)0x53);
+    Wire.requestFrom((uint8_t)0x53, 1); // Request 1 Byte
+
+    if (Wire.available())
+    {
+        adxlReg = Wire.read();
+    }
+
+    Wire.endTransmission();
+}
+
+/** 
+ * @brief  
+ * @note   
+ * @param  addr: 
+ * @param  data: 
+ * @retval None
+ */
+void Accel::writeToAddress(uint8_t addr, uint8_t data)
+{
+    Wire.beginTransmission((uint8_t)0x53);
+    Wire.write(addr); // Slave address to be written to
+
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
+/** 
+ * @brief  
+ * @note   
+ * @retval None
+ */
+void Accel::readFifo(void)
+{
+
+    for (int i = 0; i < ADXL_WATERMARK_SIZE; i++)
+    {
+        Wire.beginTransmission((uint8_t)0x53); // Begin transmission with ADXL
+        Wire.write((uint8_t)0x32);             // 0x32 is DATAX0
+        Wire.endTransmission();
+
+        Wire.beginTransmission((uint8_t)0x53);
+        Wire.requestFrom((uint8_t)0x53, 6); // Request 6 Bytes
+
+        // Read X, Y, Z values from I2c
+        for (int j = 0; j < 6; j++)
+        {
+            if (Wire.available())
+            {
+                sampleBuffer[j] = Wire.read();
+            }
+        }
+
+        Wire.endTransmission();
+
+        // Piece bytes together
+        for (int j = 0; j < 3; j++)
+        {
+            rawSample[j] = (int16_t)((((int16_t)sampleBuffer[2 * j + 1]) << 8) | sampleBuffer[2 * j]);
+        }
+
+        // do magnitude calculation
+        sampleMagnitude[i] = (int16_t)sqrt(rawSample[0] * rawSample[0] + rawSample[1] * rawSample[1] + rawSample[2] * rawSample[2]);
+    }
+    
+    adxl->getInterruptSource(); // reading this seems to allow for the water mark interrupt to happen
+    processStepCount();
+}
+
+/** 
+ * @brief  
+ * @note   
+ * @retval None
+ */
+void Accel::processStepCount(void)
+{
+    
+}
+
+
+/** 
+ * @brief  Singleton Manager.
+ * @note   
+ * @retval 
  */
 Accel *Accel::getInstance()
 {
@@ -119,78 +239,21 @@ bool Accel::setAccelRange(uint8_t accelRange)
     return isValidRange;
 }
 
+/** 
+ * @brief  
+ * @note   
+ * @param  *accelArray: 
+ * @retval None
+ */
 void Accel::readAccel(int *accelArray)
 {
     adxl->readAccel(accelArray);
 }
 
-void Accel::setupFIFO(uint8_t mode, uint8_t watermark)
+/**
+ * Deconstructor
+ */
+Accel::~Accel(void)
 {
-    // Set the mode and watermark
-    writeToAddress((uint8_t)0x38, ((uint8_t)(mode << 6) | (uint8_t)(watermark & 0x1F)) & ~(uint8_t)(0x20));
-}
-
-void Accel::readFromAddress(uint8_t addr)
-{
-    Wire.beginTransmission((uint8_t)0x53); // Begin transmission with ADXL
-    Wire.write((uint8_t)addr);             // Slave address to be read from
-    Wire.endTransmission();
-
-    Wire.beginTransmission((uint8_t)0x53);
-    Wire.requestFrom((uint8_t)0x53, 1); // Request 1 Byte
-
-    if (Wire.available())
-    {
-        adxlReg = Wire.read();
-    }
-
-    Wire.endTransmission();
-}
-
-void Accel::writeToAddress(uint8_t addr, uint8_t data)
-{
-    Wire.beginTransmission((uint8_t)0x53);
-    Wire.write(addr); // Slave address to be written to
-
-    Wire.write(data);
-    Wire.endTransmission();
-}
-
-void Accel::readFifo(void)
-{
-
-    for (int i = 0; i < ADXL_WATERMARK_SIZE; i++)
-    {
-        Wire.beginTransmission((uint8_t)0x53); // Begin transmission with ADXL
-        Wire.write((uint8_t)0x32);             // 0x32 is DATAX0
-        Wire.endTransmission();
-
-        Wire.beginTransmission((uint8_t)0x53);
-        Wire.requestFrom((uint8_t)0x53, 6); // Request 6 Bytes
-
-        // Read X, Y, Z values from I2c
-        for (int j = 0; j < 6; j++)
-        {
-            if (Wire.available())
-            {
-                sampleBuffer[j] = Wire.read();
-            }
-        }
-
-        Wire.endTransmission();
-
-        // Piece bytes together
-        for (int j = 0; j < 3; j++)
-        {
-            rawSample[j] = (int16_t)((((int16_t)sampleBuffer[2 * j + 1]) << 8) | sampleBuffer[2 * j]);
-        }
-
-        // do magnitude calculation
-        sampleMagnitude[i] = (int16_t)sqrt(rawSample[0] * rawSample[0] + rawSample[1] * rawSample[1] + rawSample[2] * rawSample[2]);
-    }
-    adxl->getInterruptSource(); // reading this seems to allow for the water mark interrupt to happen
-}
-
-void processStepCount(void)
-{
+    delete adxl;
 }
